754f743d5dd3c5898e0ec8dd3cf4d0675809d57a
User <uljana555@mail.ru>
2024-02-03 17:07:35 -0500

    day 9-10 tasks

--- src/additionall_day_practice_tasks/ATMSimulation.java
+++ src/additionall_day_practice_tasks/ATMSimulation.java
@@ -23,19 +23,16 @@ public class ATMSimulation {
                 System.err.println("Your card is locked. Please contact with your local bank");}
 
         if (scan == pin){
-            System.out.println("Select from these options:\n\tCheck Balance\n\tDeposit Money\n\tWithdraw Money");
-            String option = input.next();
+            System.out.println("Select from these options:\n\t1.Check Balance\n\t2.Deposit Money\n\t3.Withdraw Money");
+            int option = input.nextInt();
+            int choice1 = 1;
+            int choice2 = 2;
+            int choice3 = 3;
 
-            String choice1 = "Check Balance";
-            String choice2 = "Deposit Money";
-            String choice3 = "Withdraw Money";
-           // while (!(option.equalsIgnoreCase(choice1))|| option.equalsIgnoreCase(choice2)||option.equalsIgnoreCase(choice3)){
-               // System.out.println("Invalid choice. Exiting the ATM. Thank you for using our services!");
-             //   return;
-          //  }
-            if (option.equalsIgnoreCase(choice1)){
-                        System.out.println(accountBalance);
-            }else if(option.equalsIgnoreCase(choice2)){
+                     if (option==choice1){
+                         System.out.println(accountBalance);
+                          }
+            else if(option==choice2){
                         System.out.println("Enter amount to deposit:");
                         int amounttoDeposit = input.nextInt();
                         if (amounttoDeposit > 0) {
@@ -43,24 +40,69 @@ public class ATMSimulation {
                             System.out.println(totalBalance);
                         } else {
                             System.err.println("Depositing amount cannot be zero or negative, please try again later.");
-                        }
-                    }
-            else if(option.equalsIgnoreCase(choice3)){
+                        }}
+                          else if(option==choice3)
+                          {
                         System.out.println("Enter amount to withdraw:");
                         int amounttoWithdraw = input.nextInt();
                         if (amounttoWithdraw > 0 && amounttoWithdraw <= accountBalance) {
-                            int totalBalance = accountBalance - amounttoWithdraw;
-                            System.out.println(totalBalance);
+                            int totalBalance1 = accountBalance - amounttoWithdraw;
+                            System.out.println(totalBalance1);
                         } else if (amounttoWithdraw > accountBalance) {
                             System.out.println("Insufficient funds. Withdrawal failed.");
                         } else {
                             System.err.println("Cannot withdraw zero or negative amount, please try again later.");
                         }
-                    }
+
+                    }else{
+                             System.out.println("Invalid choice. Exiting the ATM. Thank you for using our services!");
+                             return;
+                     }
+                          input.close();
 
                 }
-            }}
 
+            }}
+/*
+1. Create a class named ATMSimulation. Write a program with the following requirements:
+
+   1. Given the following variables:
+      - pinNumber: contains the correct pin number of the card
+      - accountBalance: current account balance of the card
+
+   2. Ask the user to enter their pin number.
+      If the user-entered pin number does not match the correct pin number, give 2 more attempts for user to re-enter.
+      		"Incorrect Pin Number, Please re-enter:"
+
+      If all the attempts are failed, then the program should be terminated with the following error message:
+      	"Your card is locked. Please contact with your local bank"
+
+   3. If user entered the valid pin number, Ask the user to select from one of the three following options:
+      	3.1 Check Balance:
+          Displays the available balance.
+
+      	3.2 Deposit Money:
+          Asks the user to enter the amount to deposit and displays the new available balance.
+
+          If the user enters an invalid amount (0 or negative), then the program should
+          be terminated with the following error message:
+          	"Depositing amount cannot be zero or negative, please try again later."
+
+      	3.3 Withdraw Money:
+          Asks the user to enter the amount to withdraw and displays the new available balance.
+
+          If the user enters an invalid amount (0 or negative), then the program should
+          be terminated with the following error message:
+          	"Cannot withdraw zero or negative amount, please try again later."
+
+          If the user-entered amount is greater than the available balance, then the program
+          should be terminated with the following error message:
+          	"Insufficient funds. Withdrawal failed."
+
+	   	3.4 If the user does not select one of the three options above, then the following
+   			error message should be displayed on the console:
+	      		"Invalid choice. Exiting the ATM. Thank you for using our services!"
+ */
 
 
 
--- src/day03_practice_tasks/SalaryCalculator.java
+++ src/day03_practice_tasks/SalaryCalculator.java
@@ -26,3 +26,32 @@ public class SalaryCalculator {
 
     }
 }
+/*
+4. Create a class named SalaryCalculator. Given the following variables:
+				hourlyRate (double)
+				weeklyHours (int)
+				stateTaxRate (double)
+				federalTaxRate (double)
+
+	Use the given information above to calculate the following:
+				salaryBeforeTax
+				stateTax
+				federalTax
+				totalTax
+				salaryAfterTax
+
+	Display each of the above in the following format:
+
+					Example:
+						hourlyRate = $50
+						weeklyHours = 45
+						stateTaxRate = 6  (given as a percentage)
+						federalTaxRate = 26 (given as a percentage)
+
+					Output:
+						Gross pay is: $117000.0
+						Federal tax is: $30420.0
+						State tax is: $7020.0
+						Total tax is: $37440.0
+						Net income is: $79560.0
+ */
